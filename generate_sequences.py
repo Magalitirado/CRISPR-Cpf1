21af9a9954ceccefb5cd455b407617f0d9ff7b6e
Magalitirado <58011443+Magalitirado@users.noreply.github.com>
2023-04-26 13:43:51 +0200

    Update generate_sequences.py

--- generate_sequences.py
+++ generate_sequences.py
@@ -5,15 +5,12 @@ Created on Thu Mar 23 09:16:06 2023
 @author: Pro
 """
 
-import re
-    
 def generate_mismatch(grna,seq,output,mis,space):
 # grna : ARN guide
 # seq : ARN guide pour mismatch 1 et mismatch 1 pour mismatch2, ...
 # output : fichier d'écriture des résultats
 # mis : liste des mismatchs déjà obtenu pour éviter les doublons
 # space : caractère à intégrer après le résultat dans le fichier output
-    
 
     results = open(output,'a')
     for i, n in enumerate(seq):
@@ -75,35 +72,3 @@ def generate_mismatch(grna,seq,output,mis,space):
                     results.write(new+space)
                 seq[i] = 'g'
 
-# Ask for gRNA
-grna = input('Enter the sequence of gRNA: ')
-save = [grna]
-print(save)
-grna = re.findall(r'\D',grna)
-
-generate_mismatch(grna,grna,'mismatch1.txt',save,'\n')
-
-mis2 = ''.join(grna)+'\n'
-mis2 = [mis2]
-print('mis2: '+str(mis2))
-mis1 = open('mismatch1.txt','r')
-mis1 = mis1.readlines()
-
-for i, line in enumerate(mis1):
-    mis2.append(line)
-
-for line in mis1:
-    line = re.findall(r'\D',line)
-    generate_mismatch(grna,line, 'mismatch2.txt',mis2,'')
-
-mis3 = mis2
-print('mis3:'+str(mis3[:2]))
-mis2 =open('mismatch2.txt','r')
-mis2 = mis2.readlines()
-
-for i, line in enumerate(mis2):
-    mis3.append(line)
-    
-for line in mis2:
-    line = re.findall(r'\D',line)
-    generate_mismatch(grna,line,'mismatch3.txt',mis3,'')
