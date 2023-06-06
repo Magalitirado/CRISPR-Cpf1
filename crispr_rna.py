eebfb8342ef05c3e5da6d943d26ea869664626af
Magalitirado <58011443+Magalitirado@users.noreply.github.com>
2023-06-06 14:43:02 +0200

    crispr_rna.py

--- crispr_rna.py
+++ crispr_rna.py
@@ -1,37 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 27 13:30:50 2023
-
-@author: Pro
-"""
-
-def search_cpf1_grna(file):
-    # Open the genomic text file
-    with open(file, 'r',) as gen_file:
+def search_crrna(genome,pam):
+       
+    grnas = []
         
         # Open file to write results in
-        results = open('crispr_rna.txt','w')
-        
-        # Read in the entire file as a string
-        genomic_sequence = gen_file.read()
-    
-        # Get motifs
-        motifs = ['tta','ttc','ttg']
+    results = open('crispr_rna.txt','w')
     
         # Find all occurrences of the motif in the genomic sequence
-        mini = int(input('Where to begin:'))
-        maxi = int(input('Where to stop:'))
-        for motif in motifs:
-            results.write(motif.upper()+'\n\n')
-            motif_positions = []
-            pos = genomic_sequence.find(motif)
-            while pos >= 0:
-                motif_positions.append(pos)
-                pos = genomic_sequence.find(motif, pos+1)
+    mini = int(input('\nWhere to begin: '))
+    maxi = int(input('Where to stop: '))
+    results.write('\nLooking from '+str(mini)+' to '+str(maxi)+'\n\n')
+    for motif in pam:
+        motif_positions = []
+        pos = genome.find(motif)
+        while pos >= 0:
+            motif_positions.append(pos)
+            pos = genome.find(motif, pos+1)
    
-            # Print out the results
-            if len(motif_positions) > 0:
-                for pos in motif_positions:
-                    if pos > mini and pos < maxi:
-                        results.write(str(pos)+'\t'+genomic_sequence[pos:pos+28]+'\n')
-   
+        # Print out the results
+        if len(motif_positions) > 0:
+            for pos in motif_positions:
+                if pos > mini and pos < maxi:
+                    results.write(str(pos)+'\t'+genome[pos:pos+28]+'\n')
+                    grnas.append(genome[pos:pos+28])
+    return grnas   
